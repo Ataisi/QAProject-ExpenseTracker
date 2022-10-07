2844d7401c3c1e9bc76b347c0bb1c8bc87af65f5
Ataisi <ataisi.fubara@gmail.com>
2022-10-07 21:15:02 +0100

    update

--- application/routes.py
+++ application/routes.py
@@ -1,47 +1,15 @@
-from sqlalchemy import null
-from application import app, db
+from application import app
 from flask import render_template, request, url_for, redirect
-#from application.forms import AddExpenseForm
-#from application.models import Category, Expenses
-from datetime import date
 
 @app.route('/')
-@app.route('/home')
-def home():
+def home_page():
     return render_template('home.html')
 
-# routing to the add expense page
 @app.route('/addexpense')
-def addexpense():
+def add_expense():
     return render_template('addexpense.html')
 
-# @app.route('/addentry', methods = ['POST', 'GET'])
-# def add_entry():
-#     message = ""
-#     form = AddExpenseForm(request.form)
-
-#     if request.method == "POST":
-#         purchase_date = request.form['purchase_date']
-#         item = request.form['item']
-#         description = request.form['description']
-#         category = request.form['category']
-#         quantity = request.form['quantity']
-#         unit_price = request.form['unit_price']
-#         amount = request.form['amount']
-
-#         if amount == null and unit_price != null:
-#             return amount == unit_price * quantity
-#         elif amount != null and unit_price != null:
-#             return amount == unit_price * quantity
-#         else:
-#             amount < 0 and unit_price < 0
-#             amount == unit_price * quantity
-
-
-
-
-
 @app.route('/viewexpense')
-def viewexpense():
+def view_expense():
     return render_template('viewexpense.html')
 
