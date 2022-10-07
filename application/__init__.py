7d6cbaa433a28b10b6e6256ae106dbb10356d1c4
Ataisi <ataisi.fubara@gmail.com>
2022-10-07 21:24:30 +0100

    db fixes

--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # adding configuration for using a sqlite database
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://data.db"
 app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
 
 # Creating an SQLAlchemy instance
