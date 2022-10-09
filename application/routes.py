7d1a88bacc287099f7447a1d1d457d29f3e7ef5c
Ataisi <ataisi.fubara@gmail.com>
2022-10-09 20:28:35 +0100

    post

--- application/routes.py
+++ application/routes.py
@@ -1,4 +1,4 @@
-from unicodedata import category
+
 from application import app
 from flask import render_template, request, url_for, redirect
 from datetime import date
@@ -8,7 +8,7 @@ from application.models import Expenses, Category
 
 @app.route('/')
 def home_page():
-    return render_template('home.html')
+     return render_template('home.html')
 
 
 
@@ -19,7 +19,7 @@ def home_page():
 def add_expense():
     if request.method == 'GET':
         return render_template('addexpense.html')
-    print(request.form['purchased_date'])
+   
  # method to add a new expense entry
     if request.method == 'POST':
         purchase_date = request.form['purchased_date']
@@ -33,7 +33,7 @@ def add_expense():
         new_expense = Expenses(purchase_date=purchase_date, item=item, description=description, category_id = category_name, quantity=quantity, unit_price=unit_price, amount=amount)
         db.session.add(new_expense)
         db.session.commit()
-        return redirect('/')   #clear page for new entry
+        return render_template('/addexpense.html')   #clear page for new entry
 
 # view expenses
 @app.route('/viewexpense')
@@ -41,22 +41,41 @@ def view_expense():
     display_expenses = Expenses.query.all()
     return render_template('viewexpense.html', data=enumerate(display_expenses,1))
 
-# add expenses test
-@app.route('/test')
-def test():
-    purchase_date = '2020-10-20'
-    item = 'rice'
-    description = '20kg from Tesco'
-    category_name = 1
-    quantity = 1
-    unit_price = 5
-    amount = 5
+# # add expenses test
+# @app.route('/update/<int:id>', methods=['GET', 'POST'])
+# def update():
+#     if request.method == 'GET':
+#         exp = Expenses.query.filter_by(id=id).first()
+#         #if request.method=='POST':
+#         if exp:
+#             return render_template('update.html', expense=exp)
+#         else:
+#             return abort(404)
+   
+#  # method to add a new expense entry
+#     if request.method == 'POST':
+#         purchase_date = request.form['purchased_date']
+#         item = request.form['item']
+#         description = request.form['description']
+#         category_name = request.form['category']
+#         quantity = request.form['quantity']
+#         unit_price = request.form['unit_price']
+#         amount = request.form['amount']
 
-    new_expense = Expenses(purchase_date=purchase_date, item=item, description=description, category_id = category_name, quantity=quantity, unit_price=unit_price, amount=amount)
-    db.session.add(new_expense)
-    db.session.commit()
-    # cc = Category(category_name='test')
-    # Category.query.all()
-    return "<h1>done!<h1>"
+#         new_expense = Expenses(purchase_date=purchase_date, item=item, description=description, category_id = category_name, quantity=quantity, unit_price=unit_price, amount=amount)
+#         db.session.add(new_expense)
+#         db.session.commit()
+#         return render_template('/viewexpense')   #clear page for new entry
+
+@app.route('/delete/<int:id>', methods=['GET', 'POST'])
+def delete(id):
+    exp = Expenses.query.filter_by(id=id).first()
+    #if request.method=='POST':
+    if exp:
+        db.session.delete(exp)
+        db.session.commit()
+        return redirect('/viewexpense')
+    
+    return redirect("/")
 
 
